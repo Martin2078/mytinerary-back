dcba12f4d77a914bea654c88d3421e98b2d3efba
Martin <martinleonelgalan@gmail.com>
2023-11-14 16:02:52 -0300

    citiesFixed

--- controllers/cities/getAll.js
+++ controllers/cities/getAll.js
@@ -1,41 +1,13 @@
 import City from "../../models/City.js";
 
 export default async(req,res)=>{
-    try {
-        
-        let queries={}
-        let pagination={page:1,limit:12}
-        let next=false
-        let prev=false
-        if(req.query.text){
-            queries.cityName = new RegExp("^"+(req.query.text).trim(),'i') 
-        }
-        if(req.query.page){
-            pagination.page=req.query.page
-        }
+    try {  
 
-    let cities= await City.find(queries).sort({popularity:1})
-    .skip(pagination.page > 0 ? (pagination.page-1)*pagination.limit : 0)
-    .limit(pagination.limit>0 ? pagination.limit : 0)
-    
-    
-    let amountCities = await City.find(queries)
-    amountCities=amountCities.length
-    amountCities=(amountCities/pagination.limit)
-    let maxPages=Math.ceil(amountCities)
-    if (maxPages>pagination.page) {
-        next=true
-    }
-    if (pagination.page>1) {
-        prev=true
-    }
+    let cities= await City.find().sort({popularity:1})
 
     return res.json({
         success:true,
-        response:{cities,
-        next,
-        prev,
-        maxPages},
+        response:cities,
         message: "all cities"
     })   
 
